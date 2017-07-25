8a81f7603fa542edbc345294bcb37d51b80b63c1
TienHuynh <neit.developer@gmail.com>
2017-07-25 11:47:57 +0700

    change code in web

--- index.php
+++ index.php
@@ -151,10 +151,20 @@
 </head>
 
 <body>
-<div class="container-fluid">
+<div class="container">
     <div class="row">
          <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
             <div id="chartdiv"></div>
+            <div id="recommend">
+                <div class="bs-callout bs-callout-info">
+                    <h4>Trong giai đoạn <b id="start">07-2012</b> đến <b id="end">07-2017</b> người dân nên đầu tư vào</h4>
+                    <ul>
+                        <li>Đầu tư vào vàng: <b>0%</b></li>
+                        <li>Đầu tư vào dollar: <b>0%</b></li>
+                        <li>Gửi lãi suất tiết kiệm ngân hàng: <b>100%</b></li>
+                    </ul>
+                </div>
+            </div>
     </div>
 </div>
    
