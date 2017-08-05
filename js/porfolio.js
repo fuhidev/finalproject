6266fb25acc135854e35a098b71c618e1361ce71
PhuongHieu <fuhi.dev@gmail.com>
2017-08-05 09:09:04 +0700

    .

--- js/porfolio.js
+++ js/porfolio.js
@@ -2,11 +2,11 @@ loadData('/services.php/porfolio','porfolio').then(datas=>{
     if(datas.length>0){
         const data = datas[datas.length-1];
         if(data){
-            $('#pfl_gold').innerText = data[1];
-            $('#pfl_dollar').innerText = data[2];
-            $('#pfl_bank').innerText = data[3];
-            $('#pfl_from').innerText = data[4];
-            $('#pfl_to').innerText = data[5];
+            $('#pfl_gold').text(data[1] + '%');
+            $('#pfl_dollar').text(data[2]+'%');
+            $('#pfl_bank').text(data[3]+'%');
+            $('#pfl_from').text(data[4]+'%');
+            $('#pfl_to').text(data[5]);
         }
     }
 });
