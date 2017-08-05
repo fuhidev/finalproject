0186d22914685365364f9bd289698452e4b4d49c
PhuongHieu <fuhi.dev@gmail.com>
2017-08-05 09:12:20 +0700

    ok goole

--- js/porfolio.js
+++ js/porfolio.js
@@ -2,11 +2,17 @@ loadData('/services.php/porfolio','porfolio').then(datas=>{
     if(datas.length>0){
         const data = datas[datas.length-1];
         if(data){
-            $('#pfl_gold').text(data[1] + '%');
-            $('#pfl_dollar').text(data[2]+'%');
-            $('#pfl_bank').text(data[3]+'%');
-            $('#pfl_from').text(data[4]+'%');
-            $('#pfl_to').text(data[5]);
+            $('#pfl_gold').text(data[1]*100 + '%');
+            $('#pfl_dollar').text(data[2]*100+'%');
+            $('#pfl_bank').text(data[3]*100+'%');
+            
+            let from = new Date(data[4]),
+            fromString = `${from.getDate}-${from.getMonth()}-${from.getFullYear}`;
+            $('#pfl_from').text(fromString);
+
+            let to = new Date(data[5]);
+            toString = `${to.getDate}-${to.getMonth()}-${to.getFullYear}`;
+            $('#pfl_to').text(toString);
         }
     }
 });
